716e814cad4c8ac3f7503d002fb0ab05d59bce2f
denny.k <denny.k@kakaocorp.com>
2020-05-14 22:09:15 +0900

    Social College Content Parser Added to sample module

--- iOS/NoticeWorker/SeoulNational/SeoulNationalParser/Content/SeoulNationalContentParserSC.swift
+++ iOS/NoticeWorker/SeoulNational/SeoulNationalParser/Content/SeoulNationalContentParserSC.swift
@@ -11,10 +11,46 @@ import Kanna
 
 public class SeoulNationalContentParserSC: OrganizationContentParser {
     static func parseContentEconomics(html: HTMLDocument) -> Result<NoticeContent, HTMLParseError> {
-        return .failure(.notSupported)
+        var detailHTML: String = ""
+        var attachmentList = [Attachment]()
+        
+        if let contentHTML = html.css("div[class=bbs_contents]").first?.innerHTML {
+            detailHTML = generateFilteredDetailHTML(fromHTML: contentHTML)
+        } else {
+            return .failure(.emptyContent)
+        }
+        
+        if let attachmentSection = html.css("div[class=att-file]").first {
+            for attachmentItem in attachmentSection.css("li") {
+                let link = attachmentItem.css("a").first?["href"]
+                let name = attachmentItem.css("a").first?.content
+                
+                attachmentList.append(Attachment(fileName: name, fileURL: link))
+            }
+        }
+        
+        return .success(NoticeContent(content: detailHTML, attachments: attachmentList))
     }
     
     static func parseContentAnthropology(html: HTMLDocument) -> Result<NoticeContent, HTMLParseError> {
-        return .failure(.notSupported)
+        var detailHTML: String = ""
+        var attachmentList = [Attachment]()
+        
+        if let contentTable = html.css("table[class=board_view]").first, let content = contentTable.css("div[id=content]").first?.innerHTML {
+            detailHTML = generateFilteredDetailHTML(fromHTML: content)
+        } else {
+            return .failure(.emptyContent)
+        }
+        
+        if let attachmentSection = html.css("div[class=attachment]").first {
+            for attachmentItem in attachmentSection.css("li") {
+                let link = attachmentItem.css("a").first?["href"]
+                let name = attachmentItem.css("a").first?.content
+                
+                attachmentList.append(Attachment(fileName: name, fileURL: link))
+            }
+        }
+        
+        return .success(NoticeContent(content: detailHTML, attachments: attachmentList))
     }
 }
