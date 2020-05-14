eb07fcfda3597d1637365136d6aaf0eac876b3de
denny.k <denny.k@kakaocorp.com>
2020-05-14 22:22:20 +0900

    Seoul National School Notice Parser Added

--- iOS/NoticeWorker/SeoulNational/SeoulNationalParser/Content/SeoulNationalContentParser.swift
+++ iOS/NoticeWorker/SeoulNational/SeoulNationalParser/Content/SeoulNationalContentParser.swift
@@ -11,6 +11,29 @@ import Kanna
 
 public class SeoulNationalContentParser: OrganizationContentParser {
     static func parseContentSchool(html: HTMLDocument) -> Result<NoticeContent, HTMLParseError> {
-        return .failure(.notSupported)
+        // CONTENT
+        // div[class=content]
+        
+        // ATTACHMENT
+        // div[class=download] a
+        
+        var detailHTML: String = ""
+        var attachmentList = [Attachment]()
+        
+        if let contentHTML = html.css("div[class=content]").first?.innerHTML {
+            detailHTML = generateFilteredDetailHTML(fromHTML: contentHTML)
+        } else {
+            return .failure(.emptyContent)
+        }
+        
+        for attachmentItem in html.css("div[class=download] a") {
+            // https://www.snu.ac.kr/snunow/notice/genernal?md=down&bbsidx=127378&fileidx=13508
+            let link = "https://www.snu.ac.kr\(attachmentItem["href"] ?? "")"
+            let name = attachmentItem.content
+            
+            attachmentList.append(Attachment(fileName: name, fileURL: link))
+        }
+        
+        return .success(NoticeContent(content: detailHTML, attachments: attachmentList))
     }
 }
