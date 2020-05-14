88b8af0e4efb7787e4b14c360de58cd28b3eff51
denny.k <denny.k@kakaocorp.com>
2020-05-14 22:19:12 +0900

    Education College Parser Added

--- iOS/NoticeWorker/SeoulNational/SeoulNationalParser/Content/SeoulNationalContenrParserEDU.swift
+++ iOS/NoticeWorker/SeoulNational/SeoulNationalParser/Content/SeoulNationalContenrParserEDU.swift
@@ -11,6 +11,29 @@ import Kanna
 
 public class SeoulNationalContentParserEDU: OrganizationContentParser {
     static func parseContentMath(html: HTMLDocument) -> Result<NoticeContent, HTMLParseError> {
-        return .failure(.notSupported)
+        // CONTENT
+        // div[class=postArea]
+        
+        // ATTACHMENT
+        // ITERATION : li[class=custom_file]
+        
+        var detailHTML: String = ""
+        var attachmentList = [Attachment]()
+        
+        if let contentHTML = html.css("div[class=postArea]").first?.innerHTML {
+            detailHTML = generateFilteredDetailHTML(fromHTML: contentHTML)
+        } else {
+            return .failure(.emptyContent)
+        }
+        
+        for attachmentItem in html.css("li[class=custom_file]") {
+            // http://mathed.snu.ac.kr/board/notice/view/2690/download/4426
+            let link = "http://mathed.snu.ac.kr\(attachmentItem.css("a").first?["href"] ?? "")"
+            let name = attachmentItem.css("a").first?.content
+            
+            attachmentList.append(Attachment(fileName: name, fileURL: link))
+        }
+        
+        return .success(NoticeContent(content: detailHTML, attachments: attachmentList))
     }
 }
