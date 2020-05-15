73d95dc1cbc28769d3a8c0f482f76b5337b2906b
denny.k <denny.k@kakaocorp.com>
2020-05-15 19:59:19 +0900

    [OP-056] SKKU Computer EDU List Parser Implemented and TEST Completed

--- iOS/NoticeWorker/SKKU/SKKUParser/List/SKKUListParserEDU.swift
+++ iOS/NoticeWorker/SKKU/SKKUParser/List/SKKUListParserEDU.swift
@@ -11,7 +11,46 @@ import Kanna
 
 public class SKKUListEDUParser: DeptListParser {
     static func parseComputerList(page: Int, html: String) -> Result<[Notice], HTMLParseError> {
-        
-        return .failure(.notSupported)
+        cleanList()
+        do {
+            let doc = try HTML(html: html, encoding: .utf8)
+            for product in doc.css("div[class^='board-name-list board-wrap'] li") {
+                if let titleLink = product.css("dt[class^='board-list-content-title'] a").first {
+                    let url = titleLink["href"]
+                    let title = (titleLink.content ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+                    
+                    titleList.append(title)
+                    urlList.append(url ?? "")
+                }
+                
+                for (index, contentItem) in product.css("dd[class^='board-list-content-info'] li").enumerated() {
+                    switch index {
+                    case 0:
+                        break
+                    case 1:
+                        // author
+                        authorList.append(contentItem.content ?? "NO AUTHOR")
+                    case 2:
+                        // date
+                        dateStringList.append(contentItem.content ?? "NO DATE")
+                    case 3:
+                        break
+                    default:
+                        break
+                    }
+                }
+            }
+            
+            for (index, title) in titleList.enumerated() {
+                var item = Notice(title: title, url: urlList[index])
+                item.author = authorList[index]
+                item.date = dateStringList[index]
+                item.isActive = false
+                noticeList.append(item)
+            }
+            return .success(noticeList)
+        } catch {
+            return .failure(.encoding)
+        }
     }
 }
