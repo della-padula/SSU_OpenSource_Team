d54862d731de1e8ddd0e175ae19ce6064c550782
denny.k <denny.k@kakaocorp.com>
2020-05-15 21:10:30 +0900

    [OP-057] SKKU School Notice Content Parser Error Resolved

--- iOS/NoticeWorker/SKKU/SKKUParser/Content/SKKUContentParserCommon.swift
+++ iOS/NoticeWorker/SKKU/SKKUParser/Content/SKKUContentParserCommon.swift
@@ -11,6 +11,24 @@ import Kanna
 
 public class SKKUContentParserCommon: OrganizationContentParser {
     static func parseContentSKKU(html: HTMLDocument) -> Result<NoticeContent, HTMLParseError> {
-        return .failure(.notSupported)
+        var attachmentList = [Attachment]()
+        
+        for product in html.css("ul[class=filedown_list] li") {
+            if let fileLink = product.css("a").first {
+                // https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=download&articleNo=81405&attachNo=63488
+                let url = "https://www.skku.edu/skku/campus/skk_comm/notice01.do\(fileLink["href"] ?? "")"
+                let fileName = (fileLink.content ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+                
+                attachmentList.append(Attachment(fileName: fileName, fileURL: url))
+            }
+        }
+        
+        if let contentHTML = html.css("dl[class=board-write-box board-write-box-v03]").first {
+            let content = (contentHTML.innerHTML ?? "")
+            let contentString = generateFilteredDetailHTML(fromHTML: content)
+            return .success(NoticeContent(content: contentString, attachments: attachmentList))
+        } else {
+            return .failure(.emptyContent)
+        }
     }
 }
