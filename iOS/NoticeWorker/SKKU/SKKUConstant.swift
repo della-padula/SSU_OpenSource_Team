ef49991db51f8eb5b258ec2bfd20e610d5a7b111
denny.k <denny.k@kakaocorp.com>
2020-05-15 19:08:36 +0900

    [OP-056] SKKU URL Constant Definition

--- iOS/NoticeWorker/SKKU/SKKUConstant.swift
+++ iOS/NoticeWorker/SKKU/SKKUConstant.swift
@@ -9,39 +9,39 @@
 import Foundation
 
 public enum ChungangCollegeCode: CollegeName, CaseIterable {
-    case sw = "소프트웨어대학"
+    case info = "정보통신대학"
     case bio = "생명과학대학"
     case edu = "사범대학"
     case skku = "학교"
 }
 
 public enum ChungangDept: DeptItem {
-    case SW_Software(page: Int?, keyword: String?)
+    case INFO_Information(page: Int?, keyword: String?)
     case BIO_BioMeca(page: Int?, keyword: String?)
-    case EDU_Math(page: Int?, keyword: String?)
+    case EDU_Computer(page: Int?, keyword: String?)
     case SKKU(page: Int?, keyword: String?)
     
     public func getURLString(page: Int, keyword: String?) -> String {
         switch self {
-        case .SW_Software(let page, let keyword):
-            return "https://cse.snu.ac.kr/department-notices?&keys=\(keyword ?? "")&page=\((page ?? 1) - 1)"
+        case .INFO_Information(let page, let keyword):
+            return "http://icc.skku.ac.kr/icc_new/board_list_square?listPage=\(page ?? 1)&boardName=board_notice&field=subject&keyword=\(keyword ?? "")"
         case .BIO_BioMeca(let page, let keyword):
-            return "http://econ.snu.ac.kr/announcement/notice?bt=t&bq=\(keyword ?? "")&page=\(page)"
-        case .EDU_Math(let page, let keyword):
-            return "http://anthropology.or.kr/04_notice/notice01.htm?&s_t=1&s_key=\(keyword ?? "")&Page=\(page)"
+            return "https://skb.skku.edu/biomecha/community/notice.do?mode=list&&articleLimit=10&srSearchVal=\(keyword ?? "")&article.offset=\(((page ?? 1) - 1) * 10)"
+        case .EDU_Computer(let page, let keyword):
+            return "https://comedu.skku.edu/comedu/notice.do?mode=list&&articleLimit=10&srSearchVal=\(keyword ?? "")&article.offset=\(((page ?? 1) - 1) * 10)"
         case .SKKU(let page, let keyword):
-            return "https://www.snu.ac.kr/snunow/notice/genernal?sc=y&df=&dt=&qt=b&q=\(keyword ?? "")&page=\(page)"
+            return "https://www.skku.edu/skku/campus/skk_comm/notice01.do?mode=list&&articleLimit=10&srSearchVal=\(keyword ?? "")&article.offset=\(((page ?? 1) - 1) * 10)"
         }
     }
     
     public var deptName: String {
         switch self {
-        case .SW_Software:
-            return "소프트웨어학과"
+        case .INFO_Information:
+            return "정보통신학과"
         case .BIO_BioMeca:
             return "바이오메카트로닉스학과"
-        case .EDU_Math:
-            return "수학교육과"
+        case .EDU_Computer:
+            return "컴퓨터교육과"
         case .SKKU:
             return "성균관대 공지"
         }
