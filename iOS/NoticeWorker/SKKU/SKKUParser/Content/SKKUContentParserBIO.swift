5760216d3f31cc1abf6716f031f41d2df94b3a9c
denny.k <denny.k@kakaocorp.com>
2020-05-15 20:57:56 +0900

    [OP-056][OP-057] SKKU BioMeca List Parse Error Resolved

--- iOS/NoticeWorker/SKKU/SKKUParser/Content/SKKUContentParserBIO.swift
+++ iOS/NoticeWorker/SKKU/SKKUParser/Content/SKKUContentParserBIO.swift
@@ -11,6 +11,24 @@ import Kanna
 
 public class SKKUContentParserBIO: OrganizationContentParser {
     static func parseContentBioMeca(html: HTMLDocument) -> Result<NoticeContent, HTMLParseError> {
-        return .failure(.notSupported)
+        var attachmentList = [Attachment]()
+        
+        for product in html.css("ul[class=board-view-file-wrap] li") {
+            if let fileLink = product.css("a").first {
+                // https://skb.skku.edu/biomecha/community/notice.do?mode=download&articleNo=95430&attachNo=69336
+                let url = "https://skb.skku.edu/biomecha/community/notice.do\(fileLink["href"] ?? "")"
+                let fileName = (fileLink.content ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
+                
+                attachmentList.append(Attachment(fileName: fileName, fileURL: url))
+            }
+        }
+        
+        if let contentHTML = html.css("div[class=board-view-content-wrap board-view-txt]").first {
+            let content = (contentHTML.innerHTML ?? "")
+            let contentString = generateFilteredDetailHTML(fromHTML: content)
+            return .success(NoticeContent(content: contentString, attachments: attachmentList))
+        } else {
+            return .failure(.emptyContent)
+        }
     }
 }
