9487f317b4d10842a5987f66d9342c1438a903fa
denny.k <denny.k@kakaocorp.com>
2020-05-15 19:32:44 +0900

    [OP-056] SKKU Information Notice List Parser Implemented

--- iOS/NoticeWorker/SKKU/SKKUParser/List/SKKUListParserINFO.swift
+++ iOS/NoticeWorker/SKKU/SKKUParser/List/SKKUListParserINFO.swift
@@ -11,6 +11,47 @@ import Kanna
 
 public class SKKUINFOParser: DeptListParser {
     static func parseInformationList(page: Int, html: String) -> Result<[Notice], HTMLParseError> {
-        return .failure(.notSupported)
+        cleanList()
+        do {
+            let doc = try HTML(html: html, encoding: .utf8)
+            for (index, product) in doc.css("div[class^='list-table-div'] tbody td").enumerated() {
+                switch index % 6 {
+                case 0:
+                    break
+                case 1:
+                    break
+                case 2:
+                    if let titleLink = product.css("a").first {
+                        let url = titleLink["href"]
+                        let title = titleLink.content
+                        
+                        titleList.append(title ?? "")
+                        urlList.append(url ?? "")
+                    }
+                case 3:
+                    let author = product.content ?? ""
+                    authorList.append(author)
+                case 4:
+                    let date = product.content ?? ""
+                    dateStringList.append(date)
+                case 5:
+                    break
+                default:
+                    break
+                }
+            }
+            
+            for (index, title) in titleList.enumerated() {
+                var item = Notice(title: title, url: urlList[index])
+                item.author = authorList[index]
+                item.date = dateStringList[index]
+                item.isActive = false
+                noticeList.append(item)
+            }
+            return .success(noticeList)
+        } catch let error {
+            return .failure(.encoding)
+        }
+        return .failure(.emptyContent)
     }
 }
